f06974eceb5849762039071edb79ad99420f4dfd
ΑΓΓΗΣ ΠΑΝΑΓΙΩΤΗΣ <Namkorr@gmail.com>
2024-06-26 00:00:59 +0300

    edit

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const MenuButton = document.getElementById('menubtn');
-    const sidemenu = document.getElementById("sidenav").style.width = "0px";
-    const main = document.getElementById("main").style.marginLeft = "0px";
+function toggleMenu() {
+  var sideMenu = document.getElementById('sidenav');
+  var menuButton = document.getElementById('menu');
 
-
-    MenuButton.addEventListener('click', () => {
-        if (MenuButton.textContent === 'Menu') {
-            sidemenu.style.width = "250px";
-            main.style.marginLeft = "250px"
-            MenuButton.textContent = 'Close';
-            MenuButton.classList.add('close');
-        }else{
-            sidemenu.style.width = "0px";
-            main.style.marginLeft = "0px"
-            MenuButton.textContent = 'Menu';
-            button.classList.remove('close');
-        }
-
-    });
-});
+  if (sideMenu.style.width === '250px') {
+    sideMenu.style.width = '0';
+    menuButton.textContent = 'Menu';
+  } else {
+    sideMenu.style.width = '250px';
+    menuButton.textContent = 'Close';
+  }
+}
