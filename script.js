1e2047da128e33e28bae1b7097527df8edf99b79
ΑΓΓΗΣ ΠΑΝΑΓΙΩΤΗΣ <Namkorr@gmail.com>
2024-06-26 22:55:20 +0300

    new stuff

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function toggleMenu() {
   var sideMenu = document.getElementById('sidenav');
   var menuButton = document.getElementById('menu');
+  var homelogo = document.getElementById('hmbtn')
 
   if (sideMenu.style.width === '250px') {
+    menuButton.style.left = '10px';
     sideMenu.style.width = '0';
+    homelogo.style.left = '110px';
     menuButton.textContent = 'Menu';
   } else {
     sideMenu.style.width = '250px';
+    menuButton.style.left = '260px';
+    homelogo.style.left = '360px';
     menuButton.textContent = 'Close';
   }
 }
