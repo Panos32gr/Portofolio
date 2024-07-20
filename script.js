0af986d245dc840a22220885036bf504e652bde3
ΑΓΓΗΣ ΠΑΝΑΓΙΩΤΗΣ <Namkorr@gmail.com>
2024-07-20 17:18:13 +0300

    built the contact me page with emailjs react lib added ui for the submission.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+  emailjs.init('zBAkN7Fmv44dFqA_O'); 
+
+  function sendMail(event) {
+    event.preventDefault(); 
+
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_z4phvkq", "template_87hzluf", params)
+      .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        alert("Email has been sent successfully.");
+      }, function(error) {
+        console.log('FAILED...', error);
+        alert("Email sending failed.");
+      });
+  }
+
+  document.getElementById('contact-form').addEventListener('submit', sendMail);
+});
+
+
 function toggleMenu() {
   var sideMenu = document.getElementById('sidenav');
   var menuButton = document.getElementById('menu');
