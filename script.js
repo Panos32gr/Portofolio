2e6e8a70259e231a095c1092180a07107ab99a6a
Ambraxis <Namkorr@gmail.com>
2024-08-19 15:10:48 +0300

    Added Gsap animations and some images

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-  emailjs.init('zBAkN7Fmv44dFqA_O');
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("zBAkN7Fmv44dFqA_O");
 
   function sendMail(event) {
     event.preventDefault();
@@ -7,37 +7,41 @@ document.addEventListener('DOMContentLoaded', function () {
     var params = {
       name: document.getElementById("name").value,
       email: document.getElementById("email").value,
-      message: document.getElementById("message").value
+      message: document.getElementById("message").value,
     };
 
-    emailjs.send("service_z4phvkq", "template_87hzluf", params)
-      .then(function (response) {
-        console.log('SUCCESS!', response.status, response.text);
+    emailjs.send("service_z4phvkq", "template_87hzluf", params).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
         alert("Email has been sent successfully.");
-      }, function (error) {
-        console.log('FAILED...', error);
+      },
+      function (error) {
+        console.log("FAILED...", error);
         alert("Email sending failed.");
-      });
+      }
+    );
   }
 
-  document.getElementById('contact-form').addEventListener('submit', sendMail);
+  document.getElementById("contact-form").addEventListener("submit", sendMail);
 });
 
-
 function toggleMenu() {
-  var sideMenu = document.getElementById('sidenav');
-  var menuButton = document.getElementById('menu');
-  var homelogo = document.getElementById('hmbtn')
+  var sideMenu = document.getElementById("sidenav");
+  var menuButton = document.getElementById("menu");
+  var homelogo = document.getElementById("hmbtn");
 
-  if (sideMenu.style.width === '250px') {
-    menuButton.style.left = '10px';
-    sideMenu.style.width = '0';
-    homelogo.style.left = '110px';
-    menuButton.textContent = 'Menu';
+  if (sideMenu.style.width === "250px") {
+    menuButton.style.left = "10px";
+    sideMenu.style.width = "0";
+    homelogo.style.left = "110px";
+    menuButton.textContent = "Menu";
   } else {
-    sideMenu.style.width = '250px';
-    menuButton.style.left = '260px';
-    homelogo.style.left = '360px';
-    menuButton.textContent = 'Close';
+    sideMenu.style.width = "250px";
+    menuButton.style.left = "260px";
+    homelogo.style.left = "360px";
+    menuButton.textContent = "Close";
   }
 }
+
+gsap.to(".logo img", { duration: 1.5, opacity: "1" });
+gsap.to(".iconcontainer", { duration: 1.5, left: "82%", ease: "bounce"});
