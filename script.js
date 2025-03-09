3b95d2d423f20e570ca66e34011196d156ba3a2c
Panos32gr <Namkorr@gmail.com>
2025-03-09 17:37:54 +0200

    .

--- script.js
+++ script.js
@@ -27,50 +27,52 @@ document.addEventListener("DOMContentLoaded", function () {
 
 var flag = false;
 
-
-function mini1(){
-  
-  gsap.to(".vid1",{duration: 0.5, opacity: 0});
-  gsap.to(".credentials",{duration: 0.5, opacity: 0});
-  gsap.to(".pointer", {duration: 0.5, opacity: 0});
-  gsap.to(".contact", {duration: 0.5, opacity: 0});
-  gsap.to (".aboutme",{duration: 0.5, delay: 0.7, opacity: 1});
-  
+function mini1() {
+  gsap.to(".vid1", { duration: 0.5, opacity: 0 });
+  gsap.to(".credentials", { duration: 0.5, opacity: 0 });
+  gsap.to(".pointer", { duration: 0.5, opacity: 0 });
+  gsap.to(".contact", { duration: 0.5, opacity: 0 });
+  gsap.to(".aboutme", { duration: 0.5, delay: 0.7, opacity: 1 });
 }
 
-
-
-function mini2(){
-  gsap.to(".vid1",{duration: 0.5, opacity: 0});
-  gsap.to(".aboutme",{duration: 0.5, opacity: 0});
-  gsap.to(".pointer", {duration: 0.5,opacity: 0});
-  gsap.to(".contact", {duration: 0.5, opacity: 0});
-  gsap.to (".credentials",{duration: 0.5, delay: 0.7, opacity: 1});
- 
+function mini2() {
+  gsap.to(".vid1", { duration: 0.5, opacity: 0 });
+  gsap.to(".aboutme", { duration: 0.5, opacity: 0 });
+  gsap.to(".pointer", { duration: 0.5, opacity: 0 });
+  gsap.to(".contact", { duration: 0.5, opacity: 0 });
+  gsap.to(".credentials", { duration: 0.5, delay: 0.7, opacity: 1 });
 }
 
-function mini3(){
-  gsap.to(".vid1",{duration: 0.5, opacity: 0});
-  gsap.to (".credentials",{duration: 0.5, opacity: 0});
-  gsap.to(".aboutme",{duration: 0.5, opacity: 0});
-  gsap.to(".contact", {duration: 0.5, delay: 0.7, opacity: 1});
-  gsap.to(".pointer", {duration: 0.5, delay: 0.7, opacity: 1});
+function mini3() {
+  gsap.to(".vid1", { duration: 0.5, opacity: 0 });
+  gsap.to(".credentials", { duration: 0.5, opacity: 0 });
+  gsap.to(".aboutme", { duration: 0.5, opacity: 0 });
+  gsap.to(".contact", { duration: 0.5, delay: 0.7, opacity: 1 });
+  gsap.to(".pointer", { duration: 0.5, delay: 0.7, opacity: 1 });
 }
 
 function withdraw() {
-  gsap.to (".aboutme",{opacity: 0});
-  gsap.to (".credentials",{opacity: 0});
-  gsap.to(".pointer", {duration: 0.5,opacity: 0});
-  gsap.to(".contact", {duration: 0.5, opacity: 0});
-  gsap.to(".vid1",{opacity: 1});
-  gsap.to(".container",{ duration: 1, delay: 0.7, top: "-800px"});
-
+  gsap.to(".aboutme", { opacity: 0 });
+  gsap.to(".credentials", { opacity: 0 });
+  gsap.to(".pointer", { duration: 0.5, opacity: 0 });
+  gsap.to(".contact", { duration: 0.5, opacity: 0 });
+  gsap.to(".vid1", { opacity: 1 });
+  gsap.to(".container", { duration: 1, delay: 0.7, top: "-800px" });
+  gsap.to(".welcome", { duration: 1, delay: 1.2, opacity: 1});
+  gsap.to(".logo", { duration: 1, delay: 1.2, opacity: 1});
 }
 
 function toggleSphere() {
   if (!flag) {
-    gsap.to(".container",{ duration: 1, top: "200px", ease: "bounce"});
-    gsap.to(".miniSphere1", { duration: 0.5, delay: 1.2, top: "3%", left: "30%" });
+    gsap.to(".logo", {duration: 1, opacity: 0});
+    gsap.to(".welcome", {duration: 1, opacity: 0});
+    gsap.to(".container", { duration: 1, top: "200px", ease: "bounce" });
+    gsap.to(".miniSphere1", {
+      duration: 0.5,
+      delay: 1.2,
+      top: "3%",
+      left: "30%",
+    });
     gsap.to(".miniSphere2", {
       duration: 0.5,
       delay: 1.3,
@@ -96,7 +98,7 @@ function toggleSphere() {
       top: "30%",
       left: "10%",
     });
-  
+
     withdraw();
     flag = false;
   }
