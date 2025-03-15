158b58c0c10ebc312f0082943e709bfea3c5c08b
Panos32gr <Namkorr@gmail.com>
2025-03-15 19:35:31 +0200

    fgh

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function mini1() {
   gsap.to(".vid1", { duration: 0.5, opacity: 0 });
   gsap.to(".credentials", { duration: 0.5, opacity: 0 });
@@ -31,7 +30,7 @@ function withdraw() {
   gsap.to(".vid1", { opacity: 1 });
   gsap.to(".container", { duration: 1, delay: 0.7, top: "-800px" });
   gsap.to(".welcome", { duration: 1, delay: 1.2, opacity: 1 });
-  gsap.to(".selfImage", { duration: 1, delay: 1.2,  opacity: 1 });
+  gsap.to(".selfImage", { duration: 1, delay: 1.2, opacity: 1 });
   gsap.to(".logo", { duration: 1, delay: 1.2, opacity: 1 });
 }
 
@@ -42,9 +41,8 @@ function toggleSphere() {
   console.log("Window width:", windowWidth);
   console.log("Flag before toggling:", flag);
 
-
-  if (windowWidth <= 800) {
-      if (!flag) {
+  if (windowWidth <= 630) {
+    if (!flag) {
       gsap.to(".logo", { duration: 1, opacity: 0 });
       gsap.to(".welcome", { duration: 1, opacity: 0 });
       gsap.to(".selfImage", { duration: 1, opacity: 0 });
@@ -53,76 +51,36 @@ function toggleSphere() {
         duration: 0.5,
         delay: 1.2,
         top: "3%",
-        left: "53%"
+        left: "53%",
       });
       gsap.to(".miniSphere2", {
         duration: 0.5,
         delay: 1.3,
-        top: "25%",
-        left: "53%"
+        top: "20%",
+        left: "48%",
       });
       gsap.to(".miniSphere3", {
         duration: 0.5,
         delay: 1.4,
-        top: "43%",
-        left: "34%"
+        top: "33%",
+        left: "24%",
       });
       flag = true;
     } else if (flag) {
-      gsap.to(".miniSphere1", { 
-        duration: 0.5,
-         top: "20%",
-          left: "10%"
-         });
-      gsap.to(".miniSphere2", {
-        duration: 0.5,
-        top: "20%",
-        left: "10%"
-      });
-      gsap.to(".miniSphere3", {
-        duration: 0.5,
-        top: "20%",
-        left: "10%"
-      });
-
-      withdraw();
-      flag = false;
-    }
-} else if (windowWidth <= 630) {
-    if (!flag) {
-      gsap.to(".logo", { duration: 1, opacity: 0 });
-      gsap.to(".welcome", { duration: 1, opacity: 0 });
-      gsap.to(".container", { duration: 1, top: "200px", ease: "power1.out" });
       gsap.to(".miniSphere1", {
         duration: 0.5,
-        delay: 1.2,
-        top: "3%",
-        left: "30%",
+        top: "10%",
+        left: "10%",
       });
       gsap.to(".miniSphere2", {
         duration: 0.5,
-        delay: 1.3,
-        top: "20%",
-        left: "33%"
+        top: "10%",
+        left: "10%",
       });
       gsap.to(".miniSphere3", {
         duration: 0.5,
-        delay: 1.4,
-        top: "53%",
-        left: "24%"
-      });
-      flag = true;
-    } else if (flag) {
-      gsap.to(".miniSphere1", { duration: 0.5, top: "30%", left: "10%" });
-      gsap.to(".miniSphere2", {
-        duration: 0.5,
-        top: "30%",
-        left: "10%"
-      });
-      gsap.to(".miniSphere3", {
-        duration: 0.5,
-        top: "30%",
-        left: "10%"
+        top: "10%",
+        left: "10%",
       });
 
       withdraw();
@@ -143,26 +101,30 @@ function toggleSphere() {
         duration: 0.5,
         delay: 1.3,
         top: "30%",
-        left: "33%"
+        left: "33%",
       });
       gsap.to(".miniSphere3", {
         duration: 0.5,
         delay: 1.4,
         top: "53%",
-        left: "24%"
+        left: "24%",
       });
       flag = true;
     } else if (flag) {
-      gsap.to(".miniSphere1", { duration: 0.5, top: "30%", left: "10%" });
+      gsap.to(".miniSphere1", {
+        duration: 0.5,
+        top: "30%",
+        left: "10%",
+      });
       gsap.to(".miniSphere2", {
         duration: 0.5,
         top: "30%",
-        left: "10%"
+        left: "10%",
       });
       gsap.to(".miniSphere3", {
         duration: 0.5,
         top: "30%",
-        left: "10%"
+        left: "10%",
       });
 
       withdraw();
@@ -214,14 +176,13 @@ function toggleNav3() {
 }
 
 if (window.innerWidth <= 800) {
-gsap.to(".iconcontainer", { duration: 1.5, left: "55%", ease: "bounce" });
-}else{
+  gsap.to(".iconcontainer", { duration: 1.5, left: "55%", ease: "bounce" });
+} else {
   gsap.to(".iconcontainer", { duration: 1.5, left: "82%", ease: "bounce" });
 }
 
 if (window.innerWidth <= 630) {
   gsap.to(".iconcontainer", { duration: 1.5, left: "32%", ease: "bounce" });
-  }else{
-    gsap.to(".iconcontainer", { duration: 1.5, left: "82%", ease: "bounce" });
-  }
-  
+} else {
+  gsap.to(".iconcontainer", { duration: 1.5, left: "82%", ease: "bounce" });
+}
